494b887716b19e5894e602bbaf94ab9d54ccf980
toure1802 <jeunessefcm@gmail.com>
2020-07-14 06:59:45 -0400

    question8

---
+++ mulTwolt.py
@@ -0,0 +1,11 @@
+def mul():
+
+    x= int(input("inserez la premiere valeur:"))
+    y= int(input("inserez la deuxieme valeur:"))
+    print(x*y)
+
+def main():
+    mul()
+
+if __name__=="__main__":
+    main()
